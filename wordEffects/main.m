c4d22104b246259e8ca574f04dfc807d6906dc74
ttjnguyen <ttjnguyen6@gmail.com>
2016-10-31 17:54:23 -0700

    Initial Commit

---
+++ wordEffects/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  wordEffects
+//
+//  Created by Jenny Nguyen on 2016-10-31.
+//  Copyright © 2016 LightHouseLabs. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
